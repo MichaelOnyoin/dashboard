47c27790b8fe54c0fa7ef2cde2aee81cd75189a0
MichaelOnyoin <moonyoin@gmail.com>
2024-05-06 18:55:01 +0300

    On branch master  Changes to be committed: 	modified:   app/dashboard/page.tsx 	modified:   app/lib/data.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   app/dashboard/customers/page.tsx 	modified:   app/dashboard/invoices/page.tsx 	modified:   app/dashboard/layout.tsx 	modified:   app/layout.tsx 	modified:   app/lib/data.ts 	modified:   app/lib/definitions.ts 	modified:   app/page.tsx 	modified:   app/ui/dashboard/cards.tsx 	modified:   app/ui/dashboard/latest-invoices.tsx 	modified:   app/ui/dashboard/nav-links.tsx 	modified:   app/ui/dashboard/revenue-chart.tsx 	modified:   app/ui/dashboard/sidenav.tsx 	modified:   app/ui/fonts.ts 	modified:   app/ui/global.css 	modified:   app/ui/home.module.css 	modified:   app/ui/invoices/buttons.tsx 	modified:   app/ui/invoices/create-form.tsx 	modified:   app/ui/invoices/edit-form.tsx 	modified:   app/ui/invoices/pagination.tsx 	modified:   app/ui/login-form.tsx 	modified:   app/ui/search.tsx 	modified:   package-lock.json 	modified:   package.json Untracked files: 	app/dashboard/(overview)/ 	app/dashboard/invoices/[id]/ 	app/dashboard/invoices/create/ 	app/dashboard/invoices/error.tsx 	app/favicon.ico 	app/lib/actions.ts 	app/login/ 	auth.config.ts 	auth.ts 	middleware.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -40,6 +40,7 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+      
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
