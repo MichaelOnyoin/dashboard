39eaf2cd30c4d4fc1dd8be876fd2734af026ae69
MichaelOnyoin <61825881+MichaelOnyoin@users.noreply.github.com>
2024-05-06 19:51:46 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Page() {
           </p>
 
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
